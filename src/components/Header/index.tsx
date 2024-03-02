dfafa2a48a49b456ce5a6f8a84eb42dc8daf6a90
HessamGhazi <147984009+HessamGhazi@users.noreply.github.com>
2024-03-02 00:17:08 -0800

    header back to 0

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -41,7 +41,7 @@ const Header = () => {
   return (
     <>
       <header
-        className={`header left-1 top-0 z-40 flex w-full items-center ${sticky
+        className={`header left-0 top-0 z-40 flex w-full items-center ${sticky
           ? "dark:bg-gray-dark dark:shadow-sticky-dark fixed z-[9999] bg-white !bg-opacity-80 shadow-sticky backdrop-blur-sm transition"
           : "absolute bg-transparent"
           }`}
